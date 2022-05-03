87bc63ef5fed8c37a1d21d42de18c75a0a155e8b
almukanov <ruslan@almukanov.ru>
2022-05-03 10:01:47 +0500

    change translate source server

--- src/main/java/ru/almukanov/Dao/TranslateDaoImpl.java
+++ src/main/java/ru/almukanov/Dao/TranslateDaoImpl.java
@@ -8,22 +8,21 @@ import org.springframework.stereotype.Component;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class TranslateDaoImpl implements TranslateDao{
+
     public String translate(String string) throws UnirestException, IOException {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("source=en&target=ru&q=").append(string);
-        HttpResponse<String> response = Unirest.post("https://google-translate1.p.rapidapi.com/language/translate/v2")
+        stringBuilder.append(string);
+        HttpResponse<String> response = Unirest.post("https://text-translator2.p.rapidapi.com/translate")
                 .header("content-type", "application/x-www-form-urlencoded")
-                .header("accept-encoding", "application/gzip")
-                .header("x-rapidapi-host", "google-translate1.p.rapidapi.com")
-                .header("x-rapidapi-key", "e13bd3ef2bmsh79b544909f8de94p19282ejsn68224b0fe931")
-                .body(stringBuilder.toString())
+                .header("X-RapidAPI-Host", "text-translator2.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "a69f474044msh1879edf3376dcc5p16dc40jsn099b78a44dd9")
+                .body("source_language=en&target_language=ru&text="+stringBuilder.toString())
                 .asString();
-        BufferedReader br = new BufferedReader(new InputStreamReader(response.getRawBody()));
-        String str = br. readLine();
-        JSONObject jsonObject = new JSONObject(str);
-        return jsonObject.getJSONObject("data").getJSONArray("translations").getJSONObject(0).getString("translatedText");
+        JSONObject jsonObject = new JSONObject(response.getBody());
+        return jsonObject.getJSONObject("data").getString("translatedText");
     }
 }
